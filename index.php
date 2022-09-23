c45e164bd2da5adc718bcfc53d3e6aa54bdf3693
Naqeeb <naqeeblat@gmail.com>
2022-09-23 23:06:16 +0800

    exercise2

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<html>
-    
+<!DOCTYPE html>
+<html lang="en"  dir="ltr">
+    <head>
+     <meta charset=utf-8>
+     <tittle></tittle>
+     </head>
+     <body>
+        <!--without escaping-->
+<p>This is paragraph!</p>
+<?php
+echo "<P>This is also a paragraph!</p>";
+?>
+//this is a comment!
+/*
+This is also a comment
+whick works on multiple lines!
+*/
+<p>This is paragraph!</p>
+
+     </body>
 </html>
+
+
