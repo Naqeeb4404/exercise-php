399b9e0fad507ef9508d83eb582202c301b81ac6
Naqeeb <naqeeblat@gmail.com>
2022-09-24 23:27:38 +0800

    exercise 3

---
+++ exercise3/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the Document</title>
+</head>
+
+<body>
+    
+<?php
+    echo "10+5 Hi naqeeb Handsome";
+    print "12";
+?>
+</body>
+
+</html>
