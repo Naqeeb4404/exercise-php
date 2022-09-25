a768faf8c87e7fc8fd588e4ee62926bfe6e827d8
Naqeeb <naqeeblat@gmail.com>
2022-09-25 15:47:14 +0800

    exercise8

---
+++ exercise8/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the Document</title>
+</head>
+
+<body>
+
+    <?php
+    
+   //Arthmetic Operators
+   echo 5**2;
+
+?>
+
+</body>
+
+</html>
