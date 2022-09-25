121b896371386f2101c0e33424851f454bb036eb
Naqeeb <naqeeblat@gmail.com>
2022-09-25 15:23:52 +0800

    exercise6

---
+++ exercise6/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the Document</title>
+</head>
+
+<body>
+    <?php
+    
+   
+ echo str_replace("Daniel","Jason","Hi daniel");
+
+?>
+
+</body>
+
+
+</html>
+ 
+ 
