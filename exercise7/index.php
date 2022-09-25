b209a75f1e7c37027c1a617b95d3b7419200affe
Naqeeb <naqeeblat@gmail.com>
2022-09-25 15:41:14 +0800

    exercise7

---
+++ exercise7/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the Document</title>
+</head>
+
+<body>
+    <?php
+    
+   
+//string
+$name = 'Coding is fun';
+
+//Interger
+$name = 20;
+
+//Float
+$name = 20.5784;
+
+//Boolean
+
+
+//Array
+$names = array("Daniel","Dennis","Michael");
+echo $names['1'];
+
+
+?>
+
+</body>
+
+
+</html>
+ 
