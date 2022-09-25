9225c5cc623887822a209f6266196fd425bc2a70
Naqeeb <naqeeblat@gmail.com>
2022-09-25 15:56:25 +0800

    exercise11

---
+++ exercise11/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the Document</title>
+</head>
+
+<body>
+
+    <?php
+    
+   //Increment/Decrement Operators
+   $x = 10;
+   echo  $x--;
+   echo  $x;
+   
+
+?>
+
+</body>
+
+</html>
