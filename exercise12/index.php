92161f73ed968ac80a64c7f838ebd16ef225fa08
Naqeeb <naqeeblat@gmail.com>
2022-09-25 16:01:45 +0800

    exercise12

---
+++ exercise12/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the Document</title>
+</head>
+
+<body>
+
+    <?php
+    
+   //Logical Operators
+   $x = 20;
+   $y = 20;
+   
+   if ($x == $y xor 1 == 1){
+    echo "True";
+   }
+
+?>
+
+</body>
+
+</html>
