f74cf15bfb02091b222ea84d276ace85c1f543b9
Naqeeb <naqeeblat@gmail.com>
2022-09-25 15:49:31 +0800

    exercise9

---
+++ exercise9/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the Document</title>
+</head>
+
+<body>
+
+    <?php
+    
+   //Arthmetic Operators
+   $x = 100;
+   $x /= 20;
+
+
+   echo $x;
+
+?>
+
+</body>
+
+</html>
