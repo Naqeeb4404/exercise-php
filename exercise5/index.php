80ecb4ebd30c66fd2948533d77f51ff0c675d8b8
Naqeeb <naqeeblat@gmail.com>
2022-09-25 14:00:04 +0800

    exercise5

---
+++ exercise5/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TItle of the Document</Title>>
+</head>
+
+<body>
+
+<?php
+//This gets something from the database
+     /*echo "Hi";
+     echo "Daniel";*/
+    ?> 
+    Hi saya Naqeeb
+</body>
+
+</html>
