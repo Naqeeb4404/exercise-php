e9efd923345ed4d919bf66279faf037212de585e
Naqeeb <naqeeblat@gmail.com>
2022-09-25 15:53:59 +0800

    exercise10

---
+++ exercise10/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the Document</title>
+</head>
+
+<body>
+
+    <?php
+    
+   //Comparison Operators
+   $x = 5;
+   $y = 10;
+
+   if($x <> $y) {
+    echo "True!";
+   }
+   else {
+    echo "False!";
+   }
+   
+
+?>
+
+</body>
+
+</html>
