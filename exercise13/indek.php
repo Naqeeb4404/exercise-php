873b5b3a101352d476b6d5c2ba0e0ecb1675a597
Naqeeb <naqeeblat@gmail.com>
2022-09-25 16:13:43 +0800

    exercise13

---
+++ exercise13/indek.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the Document</title>
+</head>
+
+<body>
+
+    <?php
+
+    $x = 1;
+    if($x == 1){
+        echo "Naqeeb very handsome!";
+    }
+    elseif($x == 2){
+        echo "Naqeeb kinda handsome!";
+    }
+    elseif($x == 3){
+        echo "Naqeeb kinda handsome!";
+    }
+    elseif($x == 4){
+        echo "Naqeeb very litle bit ugly!";
+    }
+  
+
+?>
+
+</body>
+
+</html>
