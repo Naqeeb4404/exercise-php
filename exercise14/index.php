d85de4df47d9201838b2b273aec16f900e4bbe6b
Naqeeb <naqeeblat@gmail.com>
2022-09-26 20:04:13 +0800

    exercise14

---
+++ exercise14/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the Document</title>
+</head>
+
+<body>
+
+    <?php
+   
+    $x = 8;
+
+    switch ($x) {
+  case 1:
+      echo "the answer is 1";
+ break;
+ case 2:
+    echo "The answer is 1";
+ break;
+case 3:
+    echo "the answer is 2";
+break;
+case 4:
+    echo "The answer is 3";
+break;
+default:
+echo "There is no answer";
+
+    }
+  
+
+?>
+
+</body>
+
+</html>
