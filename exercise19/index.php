970052dda01d190dc2f50af35e4da268d18c4dbb
Naqeeb <naqeeblat@gmail.com>
2022-09-27 21:13:56 +0800

    exercise19

---
+++ exercise19/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the Document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+  <?php
+  //loop
+
+  //While :Loop
+  //Do while Loop
+  //For Loop
+  //Foreach Loop
+
+  for($x = 0; $x <= 10; $x++){
+    echo "Hi Naqeeb handsome di bumi<br>";
+  }
+
+ ?>
+
+
+</body>
+
+</html>
