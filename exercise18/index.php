bada0101a1c1a3d36f044a0644b95dbc0edd02b3
Naqeeb <naqeeblat@gmail.com>
2022-09-27 21:11:38 +0800

    exercise18

---
+++ exercise18/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the Document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+  <?php
+  //loop
+
+  //While :Loop
+  //Do while Loop
+  //For Loop
+  //Foreach Loop
+  $x = 1;
+  do {
+    echo "hi there<br>";
+    $x++;
+  }
+  while ($x <= 5);
+
+ ?>
+
+
+</body>
+
+</html>
