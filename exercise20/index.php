7e2affaa8b0741d3ecb4f20416df75ea94ffd6f4
Naqeeb <naqeeblat@gmail.com>
2022-09-27 21:16:53 +0800

    exercise20

---
+++ exercise20/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the Document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+  <?php
+  //loop
+
+  //While :Loop
+  //Do while Loop
+  //For Loop
+  //Foreach Loop
+  $array = array("Daniel", "Jane", "Naqeeb");
+
+  foreach($array as $loopdata){
+    echo "My name is ".$loopdata. "<br>";
+  }
+
+ ?>
+
+
+</body>
+
+</html>
