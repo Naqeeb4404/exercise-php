5860bb0f13b28b4333c1d3a20c78fcbe580a12cf
Naqeeb <naqeeblat@gmail.com>
2022-09-28 20:16:06 +0800

    exercise21

---
+++ exercise21/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the Document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+  <?php
+  
+  $array = array("Daniel","Jane","Jacob","John","Naqeeb");
+
+  echo $array[2];
+  
+
+ ?>
+
+
+</body>
+
+</html>
