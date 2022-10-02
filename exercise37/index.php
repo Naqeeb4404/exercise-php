fa7c569f3c2c16d61411a80533c5ce141b7852b3
Naqeeb <naqeeblat@gmail.com>
2022-10-02 23:51:20 +0800

    exercise37

--- exercise37/index.php
+++ exercise37/index.php
@@ -9,5 +9,21 @@ include_once 'includes/dbh.inc.php';
 </head>
 <body>
 
+<?php
+  $sql = "SELECT * FROM user;";
+  $result = mysqli_query();
+  $resultCheck = mysqli_num_rows($result);
+
+
+  if($resultCheck > 0) {
+    while ($row = mysqli_fetch_assoc($result)){
+        echo $row['user_uid'] . "<br>";
+    }
+        #code...
+    }
+  }
+
+?>
+
 </body>
 </html>
