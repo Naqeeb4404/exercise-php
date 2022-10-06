f69d3632328d19399291f39085c5326c12f7b779
Naqeeb <naqeeblat@gmail.com>
2022-10-06 18:59:46 +0800

    exercise42

--- exercise41/index.php
+++ exercise41/index.php
@@ -5,4 +5,13 @@
 </head>
 <body>
     <h2>Singup</h2>
-    <form action
+    <form action="includes/singup.inc.php" method="POST">
+        <input action="text" name="first" placeholder="Firstname">
+        <input action="text" name="last" placeholder="Lastname">
+        <input action="text" name="email" placeholder="E-mail">
+        <input action="text" name="uid" placeholder="Username">
+        <input action="text" name="pwd" placeholder="Password">
+        <button type="submit" name="submit">Sing up</button>
+</form>
+</body>
+</html>
