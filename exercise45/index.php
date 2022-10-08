3ae822363ee28889e0b0649a57eb391ab063c43e
Naqeeb <naqeeblat@gmail.com>
2022-10-08 19:34:20 +0800

    exercise45

---
+++ exercise45/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <tittle></title>
+</head>
+<body>
+    <?php
+
+    $person = "Daniel Naqeeb";
+    $people = array("Daniel", "Naqeeb", "John wick");
+    echo $people[0];
+    
+
+    ?>
+    </body>
+    </html>
