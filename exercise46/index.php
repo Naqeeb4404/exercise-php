ff4cf34974b104677eef544c2095fd67475e9830
Naqeeb <naqeeblat@gmail.com>
2022-10-08 19:37:28 +0800

    exercise46

---
+++ exercise46/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <tittle></title>
+</head>
+<body>
+    <?php
+
+    $data = array("First", "Second");
+    array_push($data, "Naqeeb", 15, 23);
+    print_r($data);
+    
+
+    ?>
+    </body>
+    </html>
