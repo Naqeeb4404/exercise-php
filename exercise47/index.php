72240ff1a8ac24e6c1643803716e8729d7023b43
Naqeeb <naqeeblat@gmail.com>
2022-10-08 19:38:49 +0800

    exercise47

---
+++ exercise47/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <tittle></title>
+</head>
+<body>
+    <?php
+
+    $data = array("First", "Second");
+    array_push($data, "Naqeeb", 15, 23);
+    print_r($data);
+    $data = array("First", "Second");
+    array_push($data, "Naqeeb", 15, 23);
+    print_r($data);
+    
+    $data = array("First", "Second");
+    array_push($data, "Naqeeb", 15, 23);
+    print_r($data);
+    
+    
+
+    ?>
+    </body>
+    </html>
