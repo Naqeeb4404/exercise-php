9c993b268362f3a33fd85ee1143a0c264b3f2cb1
Naqeeb <naqeeblat@gmail.com>
2022-10-09 12:54:41 +0800

    exercise50

---
+++ exercise50/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <tittle></title>
+    </head>
+    <body>
+         <?php
+            $data = array(
+            array(1, 2, 3),
+            "John"
+            "Jane"
+            );
+            //print_r($data);
+            echo $data[0][1];
+         ?>
+    </body>
+</html>
