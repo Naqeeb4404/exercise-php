add17539b7c08196c9ebf1eced3c50de60fdc69d
Naqeeb <naqeeblat@gmail.com>
2022-10-09 12:48:48 +0800

    exercise48

--- exercise47/index.php
+++ exercise47/index.php
@@ -9,14 +9,7 @@
     $data = array("First", "Second");
     array_push($data, "Naqeeb", 15, 23);
     print_r($data);
-    $data = array("First", "Second");
-    array_push($data, "Naqeeb", 15, 23);
-    print_r($data);
-    
-    $data = array("First", "Second");
-    array_push($data, "Naqeeb", 15, 23);
-    print_r($data);
-    
+   
     
 
     ?>
---
+++ exercise48/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <tittle></title>
+    </head>
+    <body>
+        <?php
+            //Indexed arrays
+            $data = array("Daniel", "John" ,"Jane");
+            echo $data[0];
+
+            //Associative arrays
+            $data = array("first" => "Daniel", "last" => "Nielsen", "age" => 25);
+
+            //Multidimensional arrays
+            $data = array(array("Daniel", "Nielsen"), "John", "Jane");
+        ?>
+    </body>
+</html>
