1753d71ef420f145599c1cbe874e78c4ceed916f
Naqeeb <naqeeblat@gmail.com>
2022-10-09 12:51:56 +0800

    exercise49

---
+++ exercise49/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <tittle></title>
+    </head>
+    <body>
+         <?php
+            $data["first"] = "Daniel";
+            $data["last"] = "Nielsen";
+            $data ["age"] = 25;
+            print_r($data);
+         ?>
+    </body>
+</html>
