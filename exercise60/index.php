6e563c03d40afe6d970caea848db04a34701c788
Naqeeb <naqeeblat@gmail.com>
2022-10-10 16:33:07 +0800

    exercise60

---
+++ exercise60/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <tittle></title>
+    </head>
+    <body>
+<?php
+//Here is a sting
+$string = "My name is Naqeeb, Naqeeb is my name";
+
+echo preg_match("/\^.*e$/", $string);
+?>
+
+</body>
+</html>
