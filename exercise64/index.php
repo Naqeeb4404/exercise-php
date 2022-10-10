a7c16c18356ea25dad41eb3935f2cfe92a82b7e5
Naqeeb <naqeeblat@gmail.com>
2022-10-10 21:35:14 +0800

    exercise64

---
+++ exercise64/index.php
@@ -0,0 +1,39 @@
+<?php
+
+if (isset($_POST['submit'])) {
+
+    $newFileName = $_POST['filename'];
+    if ($_POST['filename']) {
+        $newFileName = "gallery";
+ } else {
+    $newFileName = strtolower(str_replace(" ", " ", $newFileName));
+}
+$imageTitle = $_POST['filetitle'];
+$imageDesc = $_POST['filedesc'];
+
+$file = $_FILES['file'];
+
+$fileName = $file["name"];
+$filetype = $file["type"];
+$filetmp_name = $file["tmp_name"];
+$fileerror = $file["error"];
+$filesize = $file["size"];
+}
+$fileExt = explode(".", $fileName);
+$fileActual = strtolower(end($fileExt));
+
+$allowed = array("jpg", "jpeg", "png");
+
+if (in_array($fileActualExt, $allowed)) {
+    if ($fileError ===0) {
+        if ($fileSize > 2000000){
+// code...
+        } else {
+            echo "File size is too big!";
+            exit();
+    }
+} else {
+    echo "You had an error!";
+    exit();
+} else {
+    echo "You need to upload a proper file type!";
