0e4c3e1948612f629085bb29bd68dafe5f2c98f1
Naqeeb <naqeeblat@gmail.com>
2022-10-10 21:20:59 +0800

    exercise63

---
+++ exercise63/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <tittle></title>
+    </head>
+    <body>
+        <header>
+    <nav>
+        <a href="cases.html" class="header-cases">Cases</a>
+</nav>
+</header>
+<main>
+    <section class="gallery-links">
+        <div class="wrapper">
+            <h2>gallery</h2>
+
+            <div class="gallery-container">
+                <a href="#">
+                    <div>
+</div>
+</a>
+<div class="gallery-upload">
+</div>
+</div>
+</section>
+
+</html>
