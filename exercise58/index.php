e1f6d8637def3f155e148e0aed76efd89eaf3f1b
Naqeeb <naqeeblat@gmail.com>
2022-10-10 16:26:57 +0800

    exercise 58

---
+++ exercise58/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <tittle>Contact form tutorial</title>
+    </head>
+    <body>
+<main>
+    <p>SEND E-MAIL</p>
+    <form class="contact-form" action="contactform.php" method="post">
+        <input type="text" name="name" placeholder="Full name">
+        <input type="text" name="mail" placeholder="Your e-Mail">
+        <input type="text" name="subject" placeholder="subject">
+        <textarea name="massage" placeholder="Message"></textarea>
+        <button type="submit" name="submit">SEND EMAIL</button>
+</main>
+
+</body>
+</html>
